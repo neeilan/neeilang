c378b7987874624b32c0ff1c209894a1aeba0d40
Neeilan Selvalingam <neeilan@outlook.com>
2019-10-30 22:17:23 -0400

    Groundwork for llvm IR generation

--- src/backends/llvm/codegen.h
+++ src/backends/llvm/codegen.h
@@ -2,16 +2,27 @@
 #define _NL_BACKENDS_LLVM_CODEGEN_H_
 
 #include <map>
+#include <memory>
+#include <string>
 
+#include "cactus_table.h"
 #include "expr.h"
 #include "visitor.h"
 
 #include "llvm/IR/Value.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
 
 using llvm::Value;
+using NamedValueTable = CactusTable<std::string, Value*>;
 
 class CodeGen : public ExprVisitor<void> {
 public:
+  CodeGen() {
+    builder = llvm::make_unique<llvm::IRBuilder<>>(ctx);
+  }
+
   Value *emit(const Expr &expr) {
     expr.accept(this);
     return nullptr;
@@ -33,6 +44,10 @@ public:
 
 private:
   std::map<const Expr *, Value *> expr_values;
+  llvm::LLVMContext ctx;
+  std::unique_ptr<llvm::IRBuilder<>> builder = nullptr;
+  std::unique_ptr<llvm::Module> module; // Owns memory for generated IR.
+  NamedValueTable named_vals;
 };
 
 #endif // _NL_BACKENDS_LLVM_CODEGEN_H_
